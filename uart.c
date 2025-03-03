aef573c6a926e4edff64691b65aa390b28d7cca8
leejunggyun <lee09260@naver.com>
2025-03-03 22:14:20 +0900

    주석 추가 및 오탈자 수정

--- uart.c
+++ uart.c
@@ -4,13 +4,21 @@ uart_t ut[UART_MAX_NUM] = {
     0,
 };
 
+/**
+ * @brief uart 구조체 초기화 하는 함수
+ *
+ * @param member 멤버를 선택하여 구조체 전역 변수 ut의 특정 인덱스 초기화
+ * @return int 실패시 -1, 성공시 1
+ */
 static int init_uart_structure(UART_MEMBER member)
 {
     int ret = 0;
-    struct termios term = {0, };
+    struct termios term = {
+        0,
+    };
     char *uart_route;
 
-    switch (member)
+    switch (member) // /dev/ttyS* 의 device character file 경로 지정
     {
     case UART1:
         uart_route = UART1_ROUTE;
@@ -36,15 +44,15 @@ static int init_uart_structure(UART_MEMBER member)
         break;
     }
 
-    ret = sem_init(&ut[member].sem, 0, 1);
-    if(ret < 0)
+    ret = sem_init(&ut[member].sem, 0, 1); // 세마포어 초기화
+    if (ret < 0)
     {
         perror("fail to init semaphore");
         return -1;
     }
 
-    ret = open(uart_route, O_RDWR | O_NOCTTY);
-    if(ret < 0)
+    ret = open(uart_route, O_RDWR | O_NOCTTY); // NOCCTY = 컨트롤 신호(ctrl + c) 무시, 읽기 쓰기 권한 부여
+    if (ret < 0)
     {
         perror("fail to open uart");
         return -1;
@@ -52,35 +60,39 @@ static int init_uart_structure(UART_MEMBER member)
 
     ut[member].uart_fd = ret;
 
-    term.c_cc[VTIME] = 0;
-    term.c_cc[VMIN] = 0;
-    term.c_cflag = B115200 | CREAD | CS8 | CLOCAL & ~CSTOPB;
-    term.c_iflag = IGNPAR;
-    
-    ret = tcflush(ut[member].uart_fd, TCIOFLUSH);
-    if(ret < 0)
+    term.c_cc[VTIME] = 0;                                    // 타임아웃 시간 0 (nonblocking)
+    term.c_cc[VMIN] = 0;                                     // 최소 문자수 0 (nonblocking)
+    term.c_cflag = B115200 | CREAD | CS8 | CLOCAL & ~CSTOPB; // 115200, 읽기 허용, 데이터 비트 8, 모뎀제어신호 무시, 스탑비트 1
+    term.c_iflag = IGNPAR;                                   // 입력 패리티 무시
+
+    ret = tcflush(ut[member].uart_fd, TCIOFLUSH); // 입출력 버퍼 모두 비우기
+    if (ret < 0)
     {
         perror("fail to flush");
         return -1;
     }
 
-    ret = tcsetattr(ut[member].uart_fd, TCSANOW, &term);
-    if(ret < 0)
+    ret = tcsetattr(ut[member].uart_fd, TCSANOW, &term); // 지금 당장 속성 적용용
+    if (ret < 0)
     {
         perror("fail to set attr");
         return -1;
     }
 
     return 1;
-
 }
 
+/**
+ * @brief uart 초기화 하는 함수
+ *
+ * @return int 실패시 -1, 성공시 1
+ */
 static int init_uart()
 {
     int ret = 0;
-    
+
     ret = init_uart_structure(UART1);
-    if(ret < 0)
+    if (ret < 0)
     {
         return -1;
     }
@@ -88,12 +100,20 @@ static int init_uart()
     return 1;
 }
 
+/**
+ * @brief uart 수신함수
+ *
+ * @param mem 사용할 uart 인덱스 번호
+ * @param recv 수신될 버퍼
+ * @param recv_size 수신 버퍼 크기
+ * @return int 실패시 -1, 성공시 1
+ */
 static int uart_recv(UART_MEMBER mem, void *recv, size_t recv_size)
 {
     int ret = 0;
-    
-    ret = read(ut[mem].uart_fd, recv, recv_size);
-    if(ret < 0)
+
+    ret = read(ut[mem].uart_fd, recv, recv_size); // read 함수 써서 rx
+    if (ret < 0)
     {
         perror("fail to read data");
         return ret;
@@ -102,12 +122,20 @@ static int uart_recv(UART_MEMBER mem, void *recv, size_t recv_size)
     return ret;
 }
 
+/**
+ * @brief uart 전송 함수
+ *
+ * @param mem 사용할 uart 인덱스 번호
+ * @param send 전송할 버퍼
+ * @param send_size 전송송 버퍼 크기
+ * @return int 실패시 -1, 성공시 1
+ */
 static int uart_send(UART_MEMBER mem, void *send, size_t send_size)
 {
     int ret = 0;
 
-    ret = write(ut[mem].uart_fd, send, send_size);
-    if(ret < 0)
+    ret = write(ut[mem].uart_fd, send, send_size); // write 써서 tx
+    if (ret < 0)
     {
         perror("fail to write data");
         return ret;
@@ -116,45 +144,59 @@ static int uart_send(UART_MEMBER mem, void *send, size_t send_size)
     return ret;
 }
 
-static int thread_uart1_rx()
+/**
+ * @brief 1번 수신 스레드
+ *
+ * @return int 실패시 -1, 성공시 1
+ */
+static void thread_uart1_rx()
 {
-    char uart_rx[UART_TEMP_BUFFER_SIZE] = {0, };
+    char uart_rx[UART_TEMP_BUFFER_SIZE] = {
+        0,
+    };
     int rx_size = 0;
 
-    while(1)
+    while (1)
     {
         memset(uart_rx, 0, sizeof(uart_rx));
         rx_size = uart_recv(UART1, uart_rx, sizeof(uart_rx));
-        if(rx_size < 0)
+        if (rx_size < 0)
         {
             FATAL("fail to recv");
         }
 
-        else if(rx_size > 0)
+        else if (rx_size > 0)
         {
             DEBUG("recv : %s", uart_rx);
         }
     }
 }
 
-static int thread_uart1_tx()
+/**
+ * @brief 1번 전송 스레드
+ *
+ * @return int 실패시 -1, 성공시 1
+ */
+static void thread_uart1_tx()
 {
-    char uart_tx[UART_TEMP_BUFFER_SIZE] = {0, };
+    char uart_tx[UART_TEMP_BUFFER_SIZE] = {
+        0,
+    };
     int tx_size = 0;
     int ret = 0;
 
-    while(1)
+    while (1)
     {
         memset(uart_tx, 0, sizeof(uart_tx));
 
         ret = scanf("%s", uart_tx);
-        if(ret != 1)
+        if (ret != 1)
         {
             FATAL("fail to scanf");
         }
 
         tx_size = uart_send(UART1, uart_tx, strlen(uart_tx));
-        if(tx_size < 0)
+        if (tx_size < 0)
         {
             FATAL("fail to recv");
         }
@@ -163,24 +205,26 @@ static int thread_uart1_tx()
 
 void start_uart_thread()
 {
-    pthread_t tid[UART_THREAD_NUM] = {0, };
-    void (*function_pointer[UART_THREAD_NUM]) = {&thread_uart1_rx, &thread_uart1_tx};
+    pthread_t tid[UART_THREAD_NUM] = {
+        0,
+    };
+    void(*function_pointer[UART_THREAD_NUM]) = {&thread_uart1_rx, &thread_uart1_tx};
     uint8_t i = 0;
     int ret = 0;
 
-    init_uart();
+    init_uart(); // 초기화
 
-    for(i = 0; i < UART_THREAD_NUM; i++)
+    for (i = 0; i < UART_THREAD_NUM; i++)
     {
-        ret = pthread_create(&tid[i], NULL, function_pointer[i], NULL);
-        if(ret < 0)
+        ret = pthread_create(&tid[i], NULL, function_pointer[i], NULL); // 스레드 만듦
+        if (ret < 0)
         {
             perror("fail to create thread");
             FATAL("is %d", i);
         }
 
-        ret = pthread_detach(tid[i]);
-        if(ret < 0)
+        ret = pthread_detach(tid[i]); // 스레드 분리
+        if (ret < 0)
         {
             perror("fail to detach thread");
             FATAL("is %d", i);
--- uart.h
+++ uart.h
@@ -13,32 +13,36 @@
 
 typedef enum
 {
-    UART1,
-    UART2,
-    UART3,
-    UART4,
-    UART5,
-    UART_MAX_NUM,
-}UART_MEMBER;
+    UART1,        /* uart1번 인덱스 */
+    UART2,        /* uart2번 인덱스 */
+    UART3,        /* uart3번 인덱스 */
+    UART4,        /* uart4번 인덱스 */
+    UART5,        /* uart5번 인덱스 */
+    UART_MAX_NUM, /* 최대 길이 */
+} UART_MEMBER;
 
-#define UART1_ROUTE "/dev/ttyS1"
-#define UART2_ROUTE "/dev/ttyS2"
-#define UART3_ROUTE "/dev/ttyS3"
-#define UART4_ROUTE "/dev/ttyS4"
-#define UART5_ROUTE "/dev/ttyS5"
+#define UART1_ROUTE "/dev/ttyS1" // uart1 경로
+#define UART2_ROUTE "/dev/ttyS2" // uart2 경로
+#define UART3_ROUTE "/dev/ttyS3" // uart3 경로
+#define UART4_ROUTE "/dev/ttyS4" // uart4 경로
+#define UART5_ROUTE "/dev/ttyS5" // uart5 경로
 
-#define FATAL(fmt, ...) printf("[\x1b[31m%s\x1b[0m]" fmt "\n", __FUNCTION__, ##__VA_ARGS__)
-#define DEBUG(fmt, ...) printf("[\x1b[35m%s\x1b[0m]" fmt "\n", __FUNCTION__, ##__VA_ARGS__)
-#define UART_TEMP_BUFFER_SIZE 1024
+#define FATAL(fmt, ...) printf("[\x1b[31m%s\x1b[0m]" fmt "\n", __FUNCTION__, ##__VA_ARGS__) // 오류 정보 프린트 함수
+#define DEBUG(fmt, ...) printf("[\x1b[35m%s\x1b[0m]" fmt "\n", __FUNCTION__, ##__VA_ARGS__) // 디버그 정보 프린트 함수
+#define UART_TEMP_BUFFER_SIZE 1024                                                          // 임시 버퍼
 
-#define UART_THREAD_NUM 2
+#define UART_THREAD_NUM 2 // 현재 uart 스레드 수
 
-typedef struct 
+typedef struct
 {
-    int uart_fd;
-    sem_t sem;
-}uart_t;
+    int uart_fd; /* uart 파일 디스크립터 */
+    sem_t sem;   /* 세마포어 */
+} uart_t;
 
+/**
+ * @brief 스레드 시작 함수
+ *
+ */
 void start_uart_thread();
 
 #endif
